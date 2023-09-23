95a2a72da8d7e2d4c4748c4d20c5cfb69dcf80b6
kariukikinyanjui <michaelkariuki7@gmail.com>
2023-09-23 12:17:39 +0000

    Implementation of the Shell sort algorithm

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,38 +1,47 @@
 #include "sort.h"
 
 /**
+ * swap - Swap two integers in an array
+ * @a: The first integer to swap
+ * @b: The second integer to swap
+ */
+void swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
  * shell_sort - Sorts an array of integers in ascending order using Shell Sort
  * @array: The array to be sorted
  * @size: Number of elements in the array
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t gap = 1;
+	size_t gap;
 	size_t i, j;
-	int temp;
 
 	if (array == NULL || size < 2)
 		return;
-	while (gap < size / 3)
-		gap = gap * 3 + 1;
 
-	while (gap > 0)
+	for (gap = 1; gap < (size / 3);)
 	{
-
+		gap = gap * 3 + 1;
+	}
+	for (; gap >= 1; gap /= 3)
+	{
 		for (i = gap; i < size; i++)
 		{
-			temp = array[i];
 			j = i;
-
-			while (j >= gap && array[j - gap] > temp)
+			while (j >= gap && array[j - gap] > array[j])
 			{
-				array[j] = array[j - gap];
+				swap(array + j, array + (j - gap));
 				j -= gap;
 			}
-			array[j] = temp;
 		}
-		if (gap != 1)
-			print_array(array, size);
-		gap = (gap - 1) / 3;
+		print_array(array, size);
 	}
 }
